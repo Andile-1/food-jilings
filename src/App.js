90bdeb0f9ae8ed49ac4dd6cdedb431cf30b880e4
Andile-1 <159252369+Andile-1@users.noreply.github.com>
2024-02-29 16:43:33 +0200

    recipe site with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import RecipeCard from "./components/RecipeCard";
+const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  // search for the recipe
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = searchApi + query;
+    const res = await fetch(url);
+    const data = await res.json();
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes();
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="container">
+      <h2>🍲Homely & Yummmy Recipes🥘</h2>
+      <SearchBar
+        isLoading={isLoading}
+        query={query}
+        setQuery={setQuery}
+        handleSubmit={handleSubmit}
+      />
+      <div className="recipes">
+        {recipes
+          ? recipes.map((recipe) => (
+              <RecipeCard key={recipe.idMeal} recipe={recipe} />
+            ))
+          : "No Results."}
+      </div>
+      <footer className="footer">
+        This project was coded by Andile Jili and is{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Andile-1/food-recipes"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://majili.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
